7eb926561be533bd85c0be985513d2c3913a9e86
Ignacio <ignacioCoriadb@gmail.com>
2022-11-06 16:23:06 -0300

    relación modelos - export modelos

--- api/src/db.js
+++ api/src/db.js
@@ -30,7 +30,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { Country, TouristActivity } = sequelize.models;
+
+Country.belongsToMany(TouristActivity, {through: "Country_TouristActivity"});
+TouristActivity.belongsToMany(Country, {through: "TouristActivity"});
+
+
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
