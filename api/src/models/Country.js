7d1c4d62858402cc5aa5b72be24418be0a897873
Ignacio <ignacioCoriadb@gmail.com>
2022-11-06 16:10:02 -0300

    Model Country

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,43 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+      validate:{ /*tres letras */
+        len: [3]
+      }
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    imgFlag:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subRegion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.STRING,
+    },
+    population: {
+      type: DataTypes.STRING,
+    } 
+
+
+
+
   });
 };
